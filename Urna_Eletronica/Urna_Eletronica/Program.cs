94924adfc6e8e786b0e7855059e362703ea1defe
André Carlos Heringer <andrejrheringer@gmail.com>
2022-11-17 14:05:26 -0300

    17/11/2022

---
+++ Urna_Eletronica/Urna_Eletronica/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Urna_Eletronica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
